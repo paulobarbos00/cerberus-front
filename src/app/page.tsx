f6e517bdb1796b83890a23121bd3baf2e8219002
ytalogabriel <ytalogabr@gmail.com>
2023-03-16 07:41:12 -0300

    Other - temporary presentation for index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link';
 import React from 'react';
 
 function Home() {
-  return <div>Hello, world!</div>;
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>
+        Cerberus project é um Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Quaerat accusantium nesciunt excepturi quos. Repudiandae rem
+        distinctio perspiciatis fuga vero odio repellat consequatur deserunt
+        rerum ad, sunt ea ut nemo maiores? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Soluta, optio. Fugit nobis nulla nemo
+        cupiditate deserunt similique itaque ipsam natus ab vitae voluptatum
+        officia maxime possimus quos, perspiciatis voluptate recusandae.
+      </p>
+      <br />
+      <br />
+      <Link href="/home" className="homeButton">
+        Acessar a Home
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
