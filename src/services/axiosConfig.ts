5dcee211d2840ace024ef3dd8685f0e9c43c97c7
ytalogabriel <ytalogabr@gmail.com>
2023-03-16 04:59:46 -0300

    FIX: page rendering before localstorage data exists

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -1,12 +1,18 @@
 import axios from 'axios';
 
+let localId;
+
+if (typeof window !== 'undefined') {
+  localId = localStorage.getItem('userLoggedId');
+}
+
 const API = axios.create({
   baseURL: 'https://us-central1-salvaesse-link.cloudfunctions.net/api',
   headers: {
     authorization: process.env.NEXT_PUBLIC_AUTHORIZATION_API_HEADER,
-    user_id: localStorage.getItem('userLoggedId')
+    user_id: localId
   },
-  timeout: 10000
+  timeout: 5000
 });
 
 export default API;
