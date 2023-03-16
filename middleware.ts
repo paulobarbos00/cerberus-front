985ab4e7163945d273afd6606974dc07b3ee92d6
MarceloSTenorio <marcelosouzatenorio@gmail.com>
2023-03-16 17:31:19 -0300

    Criação de rota privada e criação e utilização do middleware para despacho de atualizações.

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/dist/server/web/spec-extension/response';
+import { NextRequest } from 'next/server';
+
+export default function middleware(req: NextRequest) {
+  const SITE_URL = 'http://localhost:3000';
+
+  const verifyUserLogged = req.cookies.get('isUserLogged');
+  const url = req.url;
+
+  if (!verifyUserLogged && url.includes('/private')) {
+    return NextResponse.redirect(`${SITE_URL}/`);
+  }
+
+  if (verifyUserLogged && url === `${SITE_URL}/`) {
+    return NextResponse.redirect(`${SITE_URL}/home`);
+  }
+}
